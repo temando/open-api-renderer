d5125c2a335857066e1a36216103cc0cf1a39cd8
Khoa Tran <khoatran@temando.com>
2017-05-19 22:29:14 +1000

    add json visualiser

---
+++ src/components/CopyButton/CopyButton.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+import 'balloon-css';
+
+export default class CopyButton extends Component {
+  constructor(props) {
+    super(props);
+
+    this.onClick = this.onClick.bind(this);
+    this.onMouseOver = this.onMouseOver.bind(this);
+
+    this.state = {
+      tooltip: this.props.tooltip
+    };
+  }
+
+  render() {
+    return (
+      <span data-balloon={this.state.tooltip} data-balloon-pos="up"
+            onClick={this.onClick}
+            onMouseOver={this.onMouseOver}
+      >Copy</span>
+    );
+  }
+
+  onClick(e) {
+    this.setState({
+      ...this.state,
+      tooltip: 'Copied'
+    });
+
+    this.props.onCopyClick(e);
+  }
+
+  onMouseOver(e) {
+    this.setState({
+      ...this.state,
+      tooltip: this.props.tooltip
+    });
+  }
+}
+
+CopyButton.propTypes = {
+  onCopyClick: PropTypes.func,
+  tooltip: PropTypes.string
+};
