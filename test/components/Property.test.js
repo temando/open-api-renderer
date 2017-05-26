d87619e1c0e4076ec9affb531d16d026dc99bc25
Brendan Abbott <brendan.abbott@temando.com>
2017-05-26 14:17:07 +1000

    Support 'type' being an array. Closes #36

---
+++ test/components/Property.test.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import Property from './../../src/components/Property/Property';
+import renderer from 'react-test-renderer';
+import ReactShallowRenderer from 'react-test-renderer/shallow';
+
+describe('<Property />', () => {
+  it('can render a basic property', () => {
+    const tree = renderer.create(
+      <Property name={'type'} type={['string']} isRequired isLast />
+    );
+
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('can render a property with enum', () => {
+    const tree = renderer.create(
+      <Property
+        name={'packagingType'}
+        type={['string']}
+        enumValues={['box', 'carton']}
+        isRequired />
+    );
+
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('can render a property with a subtype', () => {
+    const tree = renderer.create(
+      <Property
+        name={'data'}
+        type={['array']}
+        subtype={'object'}
+        isRequired={false} />
+    );
+
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('can render a property with description', () => {
+    const shallow = new ReactShallowRenderer();
+    const tree = shallow.render(
+      <Property
+        name={'type'}
+        type={['string']}
+        description={'This is _markdown_ text'}
+        isRequired />
+    );
+
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('can render a property with multiple types', () => {
+    const tree = renderer.create(
+      <Property
+        name={'value'}
+        type={['string', 'number']}
+        isRequired={false} />
+    );
+
+    expect(tree).toMatchSnapshot();
+  });
+});
--- test/parser/open-api/schemaParser.test.js
+++ test/parser/open-api/schemaParser.test.js
@@ -2,10 +2,17 @@ import getUIReadySchema from '../../../src/parser/open-api/schemaParser';
 
 import inputSchema from './data/schemaParser/inputSchema.json';
 import expectedOutputSchema from './data/schemaParser/outputSchema.json';
+import complexInputSchema from './data/schemaParser/complexInputSchema.json';
+import complexExpectedOutputSchema from './data/schemaParser/complexOutputSchema.json';
 
 describe('getUIReadySchema', () => {
   it('returns the correct result', () => {
     const outputSchema = getUIReadySchema(inputSchema);
     expect(outputSchema).toEqual(expectedOutputSchema);
+  });
+
+  it('returns the correct result', () => {
+    const outputSchema = getUIReadySchema(complexInputSchema);
+    expect(outputSchema).toEqual(complexExpectedOutputSchema);
   });
 });
