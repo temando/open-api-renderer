d739a78cf6a97bde7bf16e7004a23ccde0c0958f
Brendan Abbott <brendan.abbott@temando.com>
2017-06-02 00:23:46 +1000

    ContentContainer is needs deep comparison

--- src/components/ContentContainer/ContentContainer.js
+++ src/components/ContentContainer/ContentContainer.js
@@ -1,9 +1,9 @@
-import React, { PureComponent } from 'react'
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
 import './ContentContainer.scss'
 
-export default class ContentContainer extends PureComponent {
+export default class ContentContainer extends Component {
   render () {
     return (
       <div className='content-container'>
