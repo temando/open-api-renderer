8e9d82deb068dafd1a0818abfa9492066c007ae9
nfour <novus.nfour@gmail.com>
2017-06-13 10:48:14 +1000

    Rewrite webpack configs, add dashboard plugin as well

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,14 +1,26 @@
-module.exports = () => {
-  return {
-    devtool: 'source-map',
-    devServer: {
-      contentBase: `${__dirname}/src`,
-      compress: false,
-      port: 8200,
-      historyApiFallback: true
-    },
-    output: {
-      path: `${__dirname}/src`
-    }
-  };
-};
+const DashboardPlugin = require('webpack-dashboard/plugin')
+const WatchMissingNodeModulesPlugin = require('react-dev-utils/WatchMissingNodeModulesPlugin')
+const webpackMerge = require('webpack-merge')
+const baseConfig = require('./webpack.config')
+
+console.log('\nwebpack.config.dev\n')
+
+module.exports = webpackMerge(baseConfig, {
+  plugins: [
+    new DashboardPlugin(),
+    new WatchMissingNodeModulesPlugin(`${__dirname}/node_modules`)
+  ],
+
+  devtool: 'source-map',
+
+  devServer: {
+    contentBase: './src',
+    compress: false,
+    port: 8200,
+    historyApiFallback: true,
+    watchOptions: { aggregateTimeout: 100 },
+    stats: { chunkModules: false }
+  },
+
+  output: { path: `${__dirname}/src` }
+})
