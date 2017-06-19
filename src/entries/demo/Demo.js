0e74ee6cf0f21fdbdfa8b655648c1115c742ee8a
nfour <novus.nfour@gmail.com>
2017-06-19 15:45:25 +1000

    Demo style tweaks

--- src/entries/demo/Demo.js
+++ src/entries/demo/Demo.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { parse as parseQuery } from 'qs'
 import OpenApiRenderer from '../OpenApiRenderer'
-import './styles.scss'
+import './Demo.scss'
 
 const definitionUrl = parseQuery(location.search.split('?')[1]).url
 
@@ -22,13 +22,13 @@ export class Demo extends React.PureComponent {
     return (
       <div className='oapi-demo'>
         <div className='oapi-header'>
-          <button onClick={this.useUrlInput}>READ</button>
           <input
             name='urlInput'
             type='text'
             defaultValue={this.state.definitionUrl}
             ref={this.setUrlInput}
           />
+          <button onClick={this.useUrlInput}>READ</button>
         </div>
         <OpenApiRenderer {...{ definitionUrl }} />
       </div>
