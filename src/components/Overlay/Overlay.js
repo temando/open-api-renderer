160ff40031e8d35e20543d75e5335da056f29693
nfour <novus.nfour@gmail.com>
2017-06-19 15:19:57 +1000

    Fix proptypes

--- src/components/Overlay/Overlay.js
+++ src/components/Overlay/Overlay.js
@@ -1,8 +1,15 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import './Overlay.scss'
 
 export default class Overlay extends React.PureComponent {
-  render ({ children }) {
+  render () {
+    const { children } = this.props
+
     return <div className='overlay'>{children}</div>
   }
+}
+
+Overlay.propTypes = {
+  children: PropTypes.node
 }
