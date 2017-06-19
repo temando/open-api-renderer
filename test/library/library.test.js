3a3b52216eb2f4842865874894c244392285d042
nfour <novus.nfour@gmail.com>
2017-06-19 11:28:31 +1000

    More testyness

--- test/library/library.test.js
+++ test/library/library.test.js
@@ -1,10 +1,14 @@
+import React from 'react'
 
 describe('The built library', () => {
   it('imports as commonjs', () => {
     const OpenApiRenderer = require('../../dist/library').default
 
-    expect(OpenApiRenderer).toBeInstanceOf(Function)
+    let component
+    expect(() => {
+      component = <OpenApiRenderer foo='bar' />
+    }).not.toThrow()
 
-    console.log(OpenApiRenderer)
+    expect(component.props).toEqual({ foo: 'bar' })
   })
 })
