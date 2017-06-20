db4e62a9af07a48a523205042db62a102e4b6fbc
nfour <novus.nfour@gmail.com>
2017-06-20 13:03:48 +1000

    Get MUI wired up, using dark theme

---
+++ src/theme.js
@@ -0,0 +1,8 @@
+import { createMuiTheme } from 'material-ui/styles'
+import createPalette from 'material-ui/styles/palette'
+
+export const theme = createMuiTheme({
+  palette: createPalette({
+    type: 'dark'
+  })
+})
