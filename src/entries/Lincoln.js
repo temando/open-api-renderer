f4745ea2f89e97184f3e9c6e363d153d4b1653fa
nfour <novus.nfour@gmail.com>
2017-06-22 12:00:09 +1000

    Remove deps

--- src/entries/Lincoln.js
+++ src/entries/Lincoln.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
-import 'typeface-roboto'
 import Base from '../containers/Base'
 
 export default class LincolnRenderer extends React.Component {
