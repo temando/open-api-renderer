67290c8cbbdaed7695ea92d061a8a7a38151d2f0
nfour <novus.nfour@gmail.com>
2017-06-22 16:25:10 +1000

    Example

---
+++ src/components/Example/Example.styles.js
@@ -0,0 +1,26 @@
+import { applyTheme } from '../../theme'
+
+export const styles = applyTheme(({ shades }) => ({
+
+  'root': {
+    backgroundColor: 'rgb(17, 17, 17)',
+    color: 'white',
+    padding: '10px'
+  },
+
+  'buttons': {
+    opacity: '0',
+    position: 'relative',
+    textAlign: 'right',
+    paddingRight: '10px',
+
+    '&:$hovered': { opacity: '1' },
+    '& > span': {
+      color: 'white',
+      padding: '2px 10px',
+      cursor: 'pointer'
+    }
+  },
+
+  hovered: {}
+}))
