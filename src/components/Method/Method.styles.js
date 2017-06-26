6b546bf1ad3475be52620fbb86927a7f1ba375cd
nfour <novus.nfour@gmail.com>
2017-06-26 11:02:29 +1000

    Method

---
+++ src/components/Method/Method.styles.js
@@ -0,0 +1,9 @@
+import { createSheet } from '../../theme'
+
+export const styles = createSheet(({ shades }) => ({
+  method: {
+    border: '1px solid $border-color',
+    margin: '0 1rem 2rem 0',
+    padding: '1rem'
+  }
+}))
