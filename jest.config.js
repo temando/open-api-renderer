ba58d3aad76f8ecd32119ea46310b00aeb691b2a
Brendan Abbott <brendan@bloodbone.ws>
2017-06-27 14:31:24 +1000

    Remove balloon-css reference from mocks.

--- jest.config.js
+++ jest.config.js
@@ -7,7 +7,6 @@ module.exports = {
     'node_modules'
   ],
   moduleNameMapper: {
-    'balloon-css': 'identity-obj-proxy',
     '\\.styles$': '<rootDir>/src/__mocks__/stylesMock.js',
     '\\.(css|scss)$': 'identity-obj-proxy',
     '\\.(jpg|jpeg|png|gif|svg)$': '<rootDir>/src/__mocks__/fileMock.js'
