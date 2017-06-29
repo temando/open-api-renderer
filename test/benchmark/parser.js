b57252d2a82b76cb56e7aa0635e7d88fb5020d4d
nfour <novus.nfour@gmail.com>
2017-06-29 15:19:03 +1000

    Tweaks

--- test/benchmark/parser.js
+++ test/benchmark/parser.js
@@ -5,6 +5,7 @@ import { join } from 'path'
 
 // Big:
 // http://open-api.s3-website-ap-southeast-2.amazonaws.com/v3/RC2/pcanary-open-api-v3.0.0-RC2.json
+
 (async () => {
   const benchDir = join(__dirname, 'snapshots')
   const createRunner = (definition) => async () => {
