9ff750b9b745c2139caf2c3b9ad3bdffccb577fb
nfour <novus.nfour@gmail.com>
2017-06-29 12:06:03 +1000

    Add more vectors

--- test/benchmark/benchmark.test.js
+++ test/benchmark/benchmark.test.js
@@ -1,21 +1,33 @@
 import { parseDefinition } from '../../src/lib/definitions'
-import definition from './definition'
+import * as definitions from './definitions'
 import { Benchmark } from './Benchmark'
 import { join } from 'path'
 
-describe('Benchmark', () => {
-  global.jasmine.DEFAULT_TIMEOUT_INTERVAL = 60000
+describe('Benchmarks', () => {
+  global.jasmine.DEFAULT_TIMEOUT_INTERVAL = 3600000
 
   const benchDir = join(__dirname, 'snapshots')
+  const createRunner = (definition) => async () => {
+    await parseDefinition({ definition, parserType: 'open-api-v3' })
+  }
 
-  it('lib/parseDefinition', async () => {
-    const bench = await new Benchmark('parseDefinition')
-      .run(async () => {
-        await parseDefinition({ definition, parserType: 'open-api-v3' })
-      })
+  describe('parseDefinition', () => {
+    it('petStore', async () => {
+      const bench = await new Benchmark('parseDefinition-petStore')
+        .run(createRunner(definitions.petStore))
 
-    console.log(bench.serialize())
+      console.log(bench.serialize())
 
-    await bench.saveSnapshot(join(benchDir, bench.key))
+      await bench.saveSnapshot(join(benchDir, bench.key))
+    })
+
+    it('temandoFull', async () => {
+      const bench = await new Benchmark('parseDefinition-temandoFull')
+        .run(createRunner(definitions.temandoFull))
+
+      console.log(bench.serialize())
+
+      await bench.saveSnapshot(join(benchDir, bench.key))
+    })
   })
 })
