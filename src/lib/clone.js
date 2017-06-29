338e4341f10e4b18b1004ce1f25072e13329c9d2
nfour <novus.nfour@gmail.com>
2017-06-29 16:33:39 +1000

    Use deepClone like before

--- src/lib/clone.js
+++ src/lib/clone.js
@@ -1,2 +1,5 @@
+import { cloneDeep } from 'lodash'
+
 /** Unsafe JSON clone */
-export const clone = (obj) => JSON.parse(JSON.stringify(obj))
+// export const clone = (obj) => JSON.parse(JSON.stringify(obj))
+export const clone = cloneDeep
