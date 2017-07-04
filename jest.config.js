e2a0a7fcc3bd10c6fd2caf176e882d9b730e9d37
Khoa Tran <quangkhoa@users.noreply.github.com>
2017-07-04 11:43:30 +1000

    Display single enum value. Closes #49 (#151)

--- jest.config.js
+++ jest.config.js
@@ -7,9 +7,9 @@ module.exports = {
     'node_modules'
   ],
   moduleNameMapper: {
-    '\\.styles$': '<rootDir>/src/__mocks__/stylesMock.js',
+    '\\.styles$': '<rootDir>/test/__mocks__/stylesMock.js',
     '\\.(css|scss)$': 'identity-obj-proxy',
-    '\\.(jpg|jpeg|png|gif|svg)$': '<rootDir>/src/__mocks__/fileMock.js'
+    '\\.(jpg|jpeg|png|gif|svg)$': '<rootDir>/test/__mocks__/fileMock.js'
   },
   moduleFileExtensions: [
     'js',
