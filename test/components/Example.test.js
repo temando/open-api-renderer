b052d6b14f20373afd1ae15c3d5f71ef4e5be6bc
Szasza Palmer <spalmer@switch.tv>
2017-10-11 14:13:53 +1100

    modifying Example rendering to conform with the standard's Example object

--- test/components/Example.test.js
+++ test/components/Example.test.js
@@ -11,9 +11,25 @@ describe('<Example />', () => {
     expect(tree).toMatchSnapshot()
   })
 
+  it('renders the summary only', () => {
+    const tree = renderer.create(
+      <Example examples={[{'summary': 'This is a test summary.'}]} />
+    )
+
+    expect(tree).toMatchSnapshot()
+  })
+
+  it('renders the description only', () => {
+    const tree = renderer.create(
+      <Example examples={[{'description': 'This is a test description.'}]} />
+    )
+
+    expect(tree).toMatchSnapshot()
+  })
+
   it('renders a string example', () => {
     let tree = renderer.create(
-      <Example examples={['<message>hi</message>']} />
+      <Example examples={[{'value': '<message>hi</message>'}]} />
     )
 
     expect(tree).toMatchSnapshot()
@@ -21,7 +37,55 @@ describe('<Example />', () => {
 
   it('renders a JSON example', () => {
     const tree = renderer.create(
-      <Example examples={[{'message': 'hi'}]} />
+      <Example examples={[{'value':{'message': 'hi'}}]} />
+    )
+
+    expect(tree).toMatchSnapshot()
+  })
+
+  it('renders a string example with a summary', () => {
+    let tree = renderer.create(
+      <Example examples={[{'summary': 'This is a test summary.', 'value': '<message>hi</message>'}]} />
+    )
+
+    expect(tree).toMatchSnapshot()
+  })
+
+  it('renders a JSON example with a summary', () => {
+    const tree = renderer.create(
+      <Example examples={[{'summary': 'This is a test summary.', 'value':{'message': 'hi'}}]} />
+    )
+
+    expect(tree).toMatchSnapshot()
+  })
+
+  it('renders a string example with a description', () => {
+    let tree = renderer.create(
+      <Example examples={[{'description': 'This is a test description.', 'value': '<message>hi</message>'}]} />
+    )
+
+    expect(tree).toMatchSnapshot()
+  })
+
+  it('renders a JSON example with a description', () => {
+    const tree = renderer.create(
+      <Example examples={[{'description': 'This is a test description.', 'value':{'message': 'hi'}}]} />
+    )
+
+    expect(tree).toMatchSnapshot()
+  })
+
+  it('renders a string example with a summary and a description', () => {
+    let tree = renderer.create(
+      <Example examples={[{'summary': 'This is a test summary.', 'description': 'This is a test description.', 'value': '<message>hi</message>'}]} />
+    )
+
+    expect(tree).toMatchSnapshot()
+  })
+
+  it('renders a JSON example with a summary and a description', () => {
+    const tree = renderer.create(
+      <Example examples={[{'summary': 'This is a test summary.', 'description': 'This is a test description.', 'value':{'message': 'hi'}}]} />
     )
 
     expect(tree).toMatchSnapshot()
