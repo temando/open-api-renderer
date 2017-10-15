1bb487e2a1068c3d0e39ffa2b3e55318c52f61da
Szasza <szasza@szasza.info>
2017-10-15 12:36:13 +1100

    changing validation Accept header from YAML to JSON due to validation service fixes

--- src/parser/open-api/v3/open-api-v3-validator.js
+++ src/parser/open-api/v3/open-api-v3-validator.js
@@ -8,10 +8,8 @@ const VALIDATOR_HOST = 'https://openapi-converter.herokuapp.com'
  * @return {Promise<boolean>}
  */
 export async function validateDefinition (definition) {
-  // TODO change this and the parsing below to JSON after
-  // https://github.com/Mermade/openapi-webconverter/issues/2 is resolved
   const headers = new Headers({
-    'Accept': 'text/yaml'
+    'Accept': 'application/json'
   })
 
   const form = new FormData()
@@ -30,11 +28,10 @@ export async function validateDefinition (definition) {
     throw new Error(`There was a problem with the validation service, please see the console and ${VALIDATOR_HOST}.`)
   }
 
-  const rawResponse = await result.text()
-  const response = yaml.safeLoad(rawResponse);
+  const response = await result.json()
 
   if (response.status === false) {
-    throw new Error(`Validation error: ` + response.message)
+    throw new Error(`Validation error, message: ${response.message}, context: ${response.context}`)
   }
 
   return true
